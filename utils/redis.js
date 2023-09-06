7fd2d34c553f573cd82e01cc18e7a2f4396a8267
JosiahMbithi001 <mbithishucks@gmail.com>
2023-09-05 20:46:41 -0700

    utils Redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,37 +1,25 @@
-#!/usr/bin/node
-
-const { createClient } = require('redis');
-const { promisify } = require('util');
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.on('error', (err) => console.log(err));
-    this.connected = false;
-    this.client.on('connect', () => {
-      this.connected = true;
-    });
+    this.client = redis.createClient();
+    this.get = promisify(this.client.get).bind(this.client);
   }
 
   isAlive() {
-    return this.connected;
+    return this.client.connected;
   }
 
   async get(key) {
-    const getAsync = promisify(this.client.get).bind(this.client);
-    const val = await getAsync(key);
-    return val;
+    return this.get(key, (err, reply) => reply);
   }
 
-  async set(key, val, dur) {
-    const setAsync = promisify(this.client.set).bind(this.client);
-    await setAsync(key, val, 'EX', dur);
+  async set(key, value, duration) {
+    return this.client.set(key, value, 'EX', duration);
   }
 
-  async del(key) {
-    const delAsync = promisify(this.client.del).bind(this.client);
-    await delAsync(key);
-  }
+  async del(key) { return this.client.del(key); }
 }
 
 const redisClient = new RedisClient();
